c51090483fbb504714c2e312af3ff9a03533c2d5
vedantagrawal647 <83942222+vedantagrawal647@users.noreply.github.com>
2022-11-26 17:01:44 +0530

    Time: 16 ms (85.81%), Space: 40.3 MB (92.58%) - LeetHub

---
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countQuadruplets(int[] nums) {
+        int u=0;
+        for(int i=0;i<nums.length-2;i++)
+        {
+            for(int j=i+1;j<nums.length-1;j++)
+            {
+                for(int k=j+1;k<nums.length;k++)
+                {
+                    for(int l=k+1;l<nums.length;l++)
+                    {
+                        if(nums[i]+nums[j]+nums[k]==nums[l])
+                        {
+                            u=u+1;   
+                        }
+                    }
+                }      
+            }
+        }
+        return u;
+    }
+}
